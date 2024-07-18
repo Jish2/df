184ec0dd3d1ae52f077b2f7bbd7dd53ca736a7e9
Joshua Goon <j.j.jappiecraft@gmail.com>
2024-07-17 20:35:11 -0700

    add nvim config

---
+++ .config/nvim/lua/plugins/lazyvim.lua
@@ -0,0 +1,20 @@
+if true then
+    return {}
+end
+
+-- return {
+--     {
+--         "ianding1/leetcode.vim",
+--         keys = {
+--             {"<leader>ll", "<cmd>LeetCodeList<cr>", desc = "list leetcode problems"},
+--             {"<leader>lt", "<cmd>LeetCodeTest<cr>", desc = "test leetcode problems"},
+--             {"<leader>ls", "<cmd>LeetCodeSubmit<cr>", desc = "submit leetcode problems"},
+--             {"<leader>li", "<cmd>LeetCodeSignIn<cr>", desc = "signin to leetcode"}
+--         },
+--         cmd = "LeetCodeSignIn",
+--         config = function()
+--             vim.g.leetcode_browser = "chrome"
+--             vim.g.leetcode_solution_filetype = "python3"
+--         end
+--     }
+-- }
