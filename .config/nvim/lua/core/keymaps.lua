e87cab211f19377b112a7635f2269c7109b25ba2
Joshua Goon <j.j.jappiecraft@gmail.com>
2025-06-14 20:08:38 -0700

    switch from lazyvim to kickstart

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,56 @@
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+local function opts(desc)
+  return { noremap = true, silent = true, desc = 'keymaps.lua: ' .. desc }
+end
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- NOTE: Some terminals have coliding keymaps or are not able to send distinct keycodes
+-- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
+-- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
+-- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
+-- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+-- delete all to black hole buffer
+vim.keymap.set('n', 'x', '"_x', opts 'send to black hole')
+vim.keymap.set('n', 'd', '"_d', opts 'send to black hole')
+vim.keymap.set('n', 'D', '"_D', opts 'send to black hole')
+vim.keymap.set('v', 'd', '"_d', opts 'send to black hole')
+
+-- <leader>d should cut
+vim.keymap.set('n', '<leader>d', '"+d', opts 'cut text')
+vim.keymap.set('n', '<leader>DD', '"+D', opts 'cut text')
+vim.keymap.set('v', '<leader>d', '"+d', opts 'cut text')
+
+-- new tab
+vim.keymap.set('n', '<leader>t', ':tabnew<CR>', opts 'creates a new tab')
---
+++ .config/nvim/lua/core/options.lua
@@ -0,0 +1,144 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = false
+
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
+
+-- Make line numbers default
+vim.opt.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+-- vim.opt.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = 'a'
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
+
+-- relative numbers
+vim.opt.relativenumber = true
+
+vim.opt.smartindent = true
+
+-- fold settings
+vim.opt.foldcolumn = '1'
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+vim.opt.foldenable = true
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+
+-- fix fold styling
+function HighlightedFoldtext()
+  local pos = vim.v.foldstart
+  local line = vim.api.nvim_buf_get_lines(0, pos - 1, pos, false)[1]
+  local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+  local parser = vim.treesitter.get_parser(0, lang)
+  local query = vim.treesitter.query.get(parser:lang(), 'highlights')
+
+  if query == nil then
+    return vim.fn.foldtext()
+  end
+
+  local tree = parser:parse({ pos - 1, pos })[1]
+  local result = {}
+  local line_pos = 0
+  local prev_range = nil
+
+  for id, node, _ in query:iter_captures(tree:root(), 0, pos - 1, pos) do
+    local name = query.captures[id]
+    local start_row, start_col, end_row, end_col = node:range()
+    if start_row == pos - 1 and end_row == pos - 1 then
+      local range = { start_col, end_col }
+      if start_col > line_pos then
+        table.insert(result, { line:sub(line_pos + 1, start_col), 'Folded' })
+      end
+      line_pos = end_col
+      local text = vim.treesitter.get_node_text(node, 0)
+      if prev_range ~= nil and range[1] == prev_range[1] and range[2] == prev_range[2] then
+        result[#result] = { text, '@' .. name }
+      else
+        table.insert(result, { text, '@' .. name })
+      end
+      prev_range = range
+    end
+  end
+
+  local end_char = vim.fn.getline(vim.v.foldend):gsub('^%s*', '')
+  local line_count = vim.v.foldend - vim.v.foldstart + 1
+
+  -- edit last element to include line num
+  result[#result][1] = result[#result][1] .. ' ... ' .. end_char .. ' (' .. line_count .. ')'
+
+  return result
+end
+
+vim.opt.foldtext = [[luaeval('HighlightedFoldtext')()]]
+vim.opt.fillchars:append { fold = ' ' }
+
+vim.opt.pumblend = 0
+
+vim.g.have_nerd_font = true
+
+-- bufferline
+vim.opt.termguicolors = true
