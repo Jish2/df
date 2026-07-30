7b2d11a81b0861e1dd664f4c0e27a5b109d3a09d
Joshua Goon <jgoon@roblox.com>
2026-07-30 13:27:21 -0400

    fix(nvim): disable treesitter in telescope previews
    Avoid Neovim 0.12 crashes caused by legacy Tree-sitter injection handlers while preserving regex preview highlighting.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -293,6 +293,9 @@ require('lazy').setup({
       require('telescope').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
+        defaults = {
+          preview = { treesitter = false },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
