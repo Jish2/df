14230e556f0300345db29bb1266aba7d373bc4a7
Joshua Goon <25201575+Jish2@users.noreply.github.com>
2026-09-17 16:55:52 -0500

    darwin: default browser is satori, and per-host not shared

--- .config/nix/modules/darwin/common.nix
+++ .config/nix/modules/darwin/common.nix
@@ -196,10 +196,6 @@
   system.configurationRevision = self.rev or self.dirtyRev or null;
   system.stateVersion = 5;
 
-  system.activationScripts.postActivation.text = ''
-    defaultbrowser chrome
-  '';
-
   # copy apps into /Applications (as finder aliases) so spotlight indexes them
   system.activationScripts.applications.text =
     let
