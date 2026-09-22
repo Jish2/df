49001182e4cb7604a4fd02d9b7596a3d54423f23
Joshua Goon <25201575+Jish2@users.noreply.github.com>
2026-09-22 12:46:29 -0700

    darwin: buildEnv pathsToLink needs a list on current nixpkgs

--- .config/nix/modules/darwin/common.nix
+++ .config/nix/modules/darwin/common.nix
@@ -259,7 +259,7 @@
       env = pkgs.buildEnv {
         name = "system-applications";
         paths = config.environment.systemPackages;
-        pathsToLink = "/Applications";
+        pathsToLink = [ "/Applications" ];
       };
     in
     pkgs.lib.mkForce ''
